d6f5fbf8ae1af4e4f98e8966a4847e33d64cbb02
王碧瑶 <2487036948@qq.com>
2020-03-20 15:07:46 +0800

    数据结构链表、二叉树复习

--- test_2020_3_15_1/test_2020_3_15_1/test.cpp
+++ test_2020_3_15_1/test_2020_3_15_1/test.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #if 0
 //输入一个链表，删除倒数第k个结点
 Node* FindkToTail(Node* head, int k)
@@ -197,7 +198,6 @@ Node* copy(Node* head)
 	}
 	return pNewNode;
 }
-#endif
 //二叉树的操作
 #include <iostream>
 using namespace std;
@@ -285,11 +285,11 @@ void preorderNOR(TreeNode* pRoot)
 {
 	TreeNode* pCur = pRoot;
 	stack<TreeNode*> s;
-	while (pCur != nullptr && !s.empty())
+	while (pCur != nullptr || !s.empty())
 	{
 		while (pCur != nullptr)
 		{
-			cout << pCur->_val << " ";
+			cout << pCur->_val<<" ";
 			s.push(pCur);
 			pCur = pCur->_pLeft;
 		}
@@ -302,7 +302,7 @@ void inorderNOR(TreeNode* pRoot)
 {
 	TreeNode* pCur = pRoot;
 	stack<TreeNode*> s;
-	while (pCur != nullptr && !s.empty())
+	while (pCur != nullptr || !s.empty())
 	{
 		while (pCur != nullptr)
 		{
@@ -310,7 +310,7 @@ void inorderNOR(TreeNode* pRoot)
 			pCur = pCur->_pLeft;
 		}
 		TreeNode* top = s.top();
-		cout << top->_val << " ";
+		cout << top->_val<<" ";
 		s.pop();
 		pCur = top->_pRight;
 	}
@@ -320,7 +320,7 @@ void postorderNOR(TreeNode* pRoot)
 	TreeNode* pCur = pRoot;
 	TreeNode* last = nullptr;
 	stack<TreeNode*> s;
-	while (pCur != nullptr && !s.empty())
+	while (pCur != nullptr || !s.empty())
 	{
 		while (pCur != nullptr)
 		{
@@ -330,7 +330,7 @@ void postorderNOR(TreeNode* pRoot)
 		TreeNode* top = s.top();
 		if (top->_pRight == nullptr || top->_pRight == last)
 		{
-			cout << top->_val << " ";
+			cout << top->_val<<" ";
 			s.pop();
 			last = top;
 		}
@@ -386,6 +386,169 @@ int GetCount2(TreeNode* pRoot)
 	int count = 0;
 	return _GetCount2(pRoot, &count);
 }
+//求二叉树的叶子结点个数
+int Getleafcount(TreeNode* pRoot)
+{
+	if (pRoot == nullptr)
+		return 0;
+	else if (pRoot->_pLeft == nullptr && pRoot->_pRight == nullptr)
+		return 1;
+	else
+		return Getleafcount(pRoot->_pLeft) + Getleafcount(pRoot->_pRight);
+}
+//求二叉树的高度
+int Max(int a, int b)
+{
+	return a >= b ? a : b;
+}
+int GetHeight(TreeNode* pRoot)
+{
+	if (pRoot == nullptr)
+		return 0;
+	int left = GetHeight(pRoot->_pLeft);
+	int right = GetHeight(pRoot->_pRight);
+	return Max(left, right) + 1;
+}
+//判断一个二叉树是否为平衡二叉树
+bool isBalanced(TreeNode* pRoot)//O(n^2)
+{
+	if (pRoot == nullptr)
+		return true;
+	int h1 = GetHeight(pRoot->_pLeft);
+	int h2 = GetHeight(pRoot->_pRight);
+	int d = abs(h1 - h2);
+	return (d <= 1) && isBalanced(pRoot->_pLeft) && isBalanced(pRoot->_pRight);
+}
+bool _isBalanced_p(TreeNode* pRoot,int* height)//O(n)
+{
+	int lIsbalance = 0;
+	int rIsbalance = 0;
+	int lheight = 0;
+	int rheight = 0;
+	if (pRoot == nullptr)
+		return true;
+	//左右子树都要平衡
+	lIsbalance = _isBalanced_p(pRoot->_pLeft, &lheight);
+	rIsbalance = _isBalanced_p(pRoot->_pRight, &rheight);
+	if (lIsbalance&&rIsbalance && abs(lheight - rheight) < 2)
+	{
+		*height = lheight>rheight ? lheight + 1 : rheight + 1;
+		return true;
+	}
+	return false;
+}
+bool isBalanced_p(TreeNode* pRoot)
+{
+	int height = 0;
+	return _isBalanced_p(pRoot, &height);
+}
+//获取二叉树第k层结点的个数
+#include <assert.h>
+int GetkLevelCount(TreeNode* pRoot,int k)
+{
+	assert(k >= 1);
+	if (k == 1)
+		return 1;//根结点，也就是递归的终止条件
+	return GetkLevelCount(pRoot->_pLeft, k - 1) + GetkLevelCount(pRoot->_pRight, k - 1);
+}
+//二叉树查找值为v的结点地址
+TreeNode* Find(TreeNode* pRoot, char v)
+{
+	if (pRoot == nullptr)
+		return nullptr;
+	else if (pRoot->_val == v)
+		return pRoot;
+	else
+	{
+		TreeNode* left=Find(pRoot->_pLeft, v);
+		if (left != nullptr)//也就是在左边找到了
+			return left;
+		return Find(pRoot->_pRight, v);
+	}
+}
+//查找指定结点的双亲节点
+TreeNode* GetParent(TreeNode* pRoot, TreeNode* node)
+{
+	TreeNode* pParent = nullptr;
+	if (pRoot == nullptr || node == nullptr)
+		return nullptr;
+	if (pRoot == node)//如果是根结点，没有双亲，返回nullptr
+		return nullptr;
+	if (pRoot->_pLeft == node || pRoot->_pRight == node)//pRoot就是它的双亲
+		return pRoot;
+	//在左子树中找，递归继续以上操作
+	pParent = GetParent(pRoot->_pLeft, node);
+	if (pParent != nullptr)//说明在左子树中找到了
+		return pParent;
+	return GetParent(pRoot->_pRight, node);
+}
+//判断二叉树是否是完全二叉树,即层序遍历程序碰到空节点，后面的节点一定是空
+bool isCompleteTree(TreeNode* pRoot)
+{
+	if (pRoot == nullptr)
+		return true;
+	queue<TreeNode*> q;
+	q.push(pRoot);
+	while (1)
+	{
+		TreeNode* front = q.front();
+		q.pop();
+		if (front == nullptr)//遇到空节点
+			break;//跳出循环
+		q.push(front->_pLeft);
+		q.push(front->_pRight);
+	}
+	//走到这里说明已经到了叶子结点并且是空节点的情况了
+	while (!q.empty())
+	{
+		TreeNode* front = q.front();
+		q.pop();
+		if (front != nullptr)
+			return false;
+	}
+	return true;
+}
+//判断二叉树镜像
+bool isMirror(TreeNode* s,TreeNode* t)
+{
+	if (s == nullptr && t == nullptr)
+		return true;
+	if (s == nullptr || t == nullptr)
+		return false;
+	return s->_val == t->_val && isMirror(s->_pLeft, t->_pRight) && isMirror(s->_pRight, t->_pLeft);
+}
+//二叉树面试题
+//检查两棵二叉树是否相同
+bool isSameTree(TreeNode* p, TreeNode* q)
+{
+	if (p == nullptr && q == nullptr)
+		return true;
+	if (p == nullptr || q == nullptr)
+		return false;
+	return p->_val == q->_val && isSameTree(p->_pLeft, q->_pLeft) && isSameTree(p->_pRight, q->_pRight);
+}
+//判断一棵树是否为另一棵树的子树
+bool isSubTree(TreeNode* p, TreeNode* q)
+{
+	if (p == nullptr)
+		return false;
+	if (p->_val == q->_val)
+	{
+		if(isSameTree(p, q))
+			return true;
+	}
+	bool left = isSubTree(p->_pLeft, q);
+	if (left == true)
+		return true;
+	return isSubTree(p->_pRight, q);
+}
+//判断对称二叉树
+bool isSymmetric(TreeNode* pRoot)
+{
+	if (pRoot == nullptr)
+		return true;
+	return isMirror(pRoot->_pLeft, pRoot->_pRight);
+}
 int main()
 {
 	char* str = "ABD##E#H##CF##G##";
@@ -411,5 +574,295 @@ int main()
 	cout << "结点个数为:" << count << endl;
 	count = GetCount2(a);
 	cout << "结点个数为:" << count << endl;
+	count = Getleafcount(a);
+	cout << "叶子结点个数是:" << count << endl;
+	count = GetHeight(a);
+	cout << "二叉树a的高度是:" << count << endl;
+	bool flag = isBalanced(a);
+	if (flag == true)
+		cout << "是平衡二叉树" << endl;
+	else
+		cout << "不是平衡二叉树" << endl;
+	flag = isBalanced_p(a);
+	if (flag == true)
+		cout << "是平衡二叉树" << endl;
+	else
+		cout << "不是平衡二叉树" << endl;
+	count = GetkLevelCount(a, 3);
+	cout << "a二叉树第3层的结点个数是:" << count << endl;
+	TreeNode* p = Find(a, 'F');
+	cout << p->_val << endl;
+	p = GetParent(a, Find(a,'F'));
+	cout << p->_val << endl;
+	flag = isCompleteTree(a);
+	if (flag == true)
+		cout << "是完全二叉树" << endl;
+	else
+		cout << "不是完全二叉树" << endl;
+	flag = isMirror(a, pNewNode);
+	if (flag == true)
+		cout << "a和pNewNode是镜像" << endl;
+	else
+		cout << "a和pNewNode不是镜像" << endl;
+	flag = isSameTree(a, pNewNode);
+	if (flag == true)
+		cout << "两棵二叉树相同" << endl;
+	else
+		cout << "两棵二叉树不相同" << endl;
+	flag = isSubTree(a, pNewNode);
+	if (flag == true)
+		cout << "是它的子树" << endl;
+	else
+		cout << "不是它的子树" << endl;
+	flag = isSymmetric(a);
+	if (flag)
+		cout << "是对称二叉树" << endl;
+	else
+		cout << "不是对称二叉树" << endl;
+	return 0;
+}
+#endif
+#if 0
+//二叉树进阶面试题
+//根据前序遍历和中序遍历建立二叉树
+struct TreeNode
+{
+	TreeNode* _pLeft;
+	TreeNode* _pRight;
+	int _val;
+	TreeNode(int val)
+		:_val(val)
+		, _pLeft(nullptr)
+		, _pRight(nullptr)
+	{}
+};
+#include <vector>
+#include <iostream>
+using namespace std;
+TreeNode* BuildTree1(vector<int> preorder, vector<int> inorder)
+{
+	int size = inorder.size();
+	if (size == 0)
+		return nullptr;
+	TreeNode* pRoot = new TreeNode(preorder[0]);//构造根结点
+	int flag = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		if (inorder[i] == preorder[0])//在中序序列中找根结点
+			flag = i;
+	}
+	vector<int> preleft, inleft, preright, inright;
+	for (int i = 0; i < flag; ++i)
+	{
+		preleft.push_back(preorder[i + 1]);
+		inleft.push_back(inorder[i]);
+	}
+	for (int i = flag + 1; i < size; ++i)
+	{
+		preright.push_back(preorder[i]);
+		inright.push_back(inorder[i]);
+	}
+	//进行递归即可
+	pRoot->_pLeft = BuildTree1(preleft, inleft);
+	pRoot->_pRight = BuildTree1(preright, inright);
+	return pRoot;
+}
+//根据中序和后序序列创建二叉树
+TreeNode* BuildTree2(vector<int> inorder, vector<int> postorder)
+{
+	int size = inorder.size();
+	if (size == 0)
+		return nullptr;
+	TreeNode* pRoot = new TreeNode(postorder[size - 1]);
+	int flag = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		if (inorder[i] == postorder[size - 1])
+		{
+			flag = i;
+			break;
+		}
+	}
+	vector<int> inleft, inright, postleft, postright;
+	for (int i = 0; i < flag; ++i)
+	{
+		inleft.push_back(inorder[i]);
+		postleft.push_back(postorder[i]);
+	}
+	for (int i = flag + 1; i < size; ++i)
+	{
+		inright.push_back(inorder[i]);
+		postright.push_back(postorder[i - 1]);
+	}
+	pRoot->_pLeft = BuildTree2(inleft, postleft);
+	pRoot->_pRight = BuildTree2(inright, postright);
+	return pRoot;
+}
+int main()
+{
+	vector<int> pre{ 1, 2, 4, 5, 8, 3, 6, 7 };
+	vector<int> in{ 4, 2, 5, 8, 1, 6, 3, 7 };
+	vector<int> post{ 4, 8, 5, 2, 6, 7, 3, 1 };
+	TreeNode* pRoot1 = BuildTree1(pre, in);
+	TreeNode* pRoot2 = BuildTree2(in, post);
+	return 0;
+}
+#endif
+//二叉树创建字符串
+#include <iostream>
+using namespace std;
+struct TreeNode
+{
+	TreeNode* _pLeft;
+	TreeNode* _pRight;
+	char _val;
+	TreeNode(char val)
+		:_val(val)
+		, _pLeft(nullptr)
+		, _pRight(nullptr)
+	{}
+};
+TreeNode* _CreateTree(char* arr, int size, int* index)
+{
+	TreeNode* pRoot = nullptr;
+	if (*index < size && arr[*index] != '#')
+	{
+		pRoot = new TreeNode(arr[*index]);
+		(*index)++;
+		pRoot->_pLeft = _CreateTree(arr, size, index);
+		(*index)++;
+		pRoot->_pRight = _CreateTree(arr, size, index);
+	}
+	return pRoot;
+}
+
+TreeNode* CreateTree(char* arr, int size)
+{
+	int index = 0;
+	return _CreateTree(arr, size, &index);
+}
+#if 0
+//创建字符串,采用前序遍历的方式将一个二叉树转换为一个由括号和整数组成的字符串，空节点用()表示
+char* _tree2str(TreeNode* pRoot, char* ret)
+{
+	if (pRoot == nullptr)
+		return ret;
+	char szvalue[32] = { 0 };
+	sprintf(szvalue, "%d", pRoot->_val);
+	strcat(ret, szvalue);//追加
+	if (nullptr == pRoot->_pLeft)//如果根结点左子树为空
+	{
+		if (nullptr == pRoot->_pRight)//右子树也为空
+		{
+			return ret;
+		}
+		else//右子树不是空，先加上()
+		{
+			strcat(ret, "()");
+		}
+	}
+	else//根结点左子树不为空
+	{
+		strcat(ret, "(");
+		_tree2str(pRoot->_pLeft, ret);//将左子树的解决完成加上右括号
+		strcat(ret, ")");
+	}
+	if (nullptr == pRoot->_pRight)//判断根结点的右子树
+		return ret;
+	else
+	{
+		strcat(ret, "(");
+		_tree2str(pRoot->_pRight, ret);
+		strcat(ret, ")");
+	}
+	return ret;
+}
+char* tree2str(TreeNode* pRoot)
+{
+	char result[100000];
+	result[0] = '\0';
+	return _tree2str(pRoot, result);
+}
+int main()
+{
+	char arr[] = { 1, 2, 4, 3};
+	int size = sizeof(arr) / sizeof(arr[0]);
+	TreeNode* a = CreateTree(arr, size);
+	char* ret = tree2str(a);
+	cout << ret << endl;
+	return 0;
+}
+#endif
+//最近公共祖先
+TreeNode* lowstCommonAncestor(TreeNode* pRoot,TreeNode* p,TreeNode* q)
+{
+	if (pRoot == p || pRoot == q || !pRoot)
+		return pRoot;//如果p和q都是根结点，则他们的最近公共祖先是根结点
+	TreeNode* left = lowstCommonAncestor(pRoot->_pLeft, p, q);
+	TreeNode* right = lowstCommonAncestor(pRoot->_pRight, p, q);
+	//在左右子树都找了之后
+	if (!left && !right)//左右子树都没有
+		return nullptr;
+	if (!left && right)//在右子树，但是左子树找不到
+		return  right;
+	if (left && !right)
+		return left;
+	return pRoot;//左右子树分居两侧
+}
+//搜索二叉树转换为双向排序链表
+void ConvertHelp(TreeNode* pRoot, TreeNode* pre)
+{
+	if (pRoot == nullptr)
+		return;
+	//按照中序遍历顺序处理
+	//先处理左子树
+	ConvertHelp(pRoot->_pLeft, pre);
+	//处理根结点
+	pRoot->_pLeft = pre;
+	if (pre)
+	{
+		pre->_pRight = pRoot;
+	}
+	//处理右子树
+	pre = pRoot;
+	ConvertHelp(pRoot->_pRight, pre);
+}
+TreeNode* Convert(TreeNode* pRoot)
+{
+	if (pRoot == nullptr)
+		return nullptr;
+	TreeNode* pre = nullptr;
+	ConvertHelp(pRoot, pre);
+	//最后要用链表第一个结点，也就是二叉树最左边的结点表示这个链表，因此要找到二叉树最左边的结点
+	TreeNode* pCur = pRoot;
+	while (pCur->_pLeft)
+	{
+		pCur = pCur->_pLeft;
+	}
+	return pCur;
+}
+TreeNode* find(TreeNode* pRoot, char val)
+{
+	if (pRoot == nullptr)
+		return nullptr;
+	else if (pRoot->_val == val)
+		return pRoot;
+	else
+	{
+		TreeNode* left = find(pRoot->_pLeft, val);
+		if (left != nullptr)
+			return left;
+		return find(pRoot->_pRight, val);
+	}
+}
+
+int main()
+{
+	char* str = "ABD##E#H##CF##G##";
+	int size = strlen(str);
+	TreeNode* a = CreateTree(str, size);
+	TreeNode* common = lowstCommonAncestor(a, find(a,'D'), find(a,'E'));
+	cout << common->_val << endl;
+	TreeNode* so = Convert(a);
 	return 0;
 }
