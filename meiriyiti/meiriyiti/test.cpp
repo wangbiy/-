c035279bd228cac9e32eb06642c16f2b3e8c7645
王碧瑶 <2487036948@qq.com>
2020-04-03 23:57:01 +0800

    每日一题练习

--- meiriyiti/meiriyiti/test.cpp
+++ meiriyiti/meiriyiti/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 #include <vector>
+#if 0
 //斐波那契凤尾，即输出第n个斐波那契数，如果超过6位数，就返回后六位即可
 int main()
 {
@@ -19,6 +20,36 @@ int main()
 			printf("%d\n", fib[n]);
 		else
 			printf("%06d\n", fib[n]);
+	}
+	return 0;
+}
+#endif
+//求出斐波那契数列中某一段的和
+#define MAXSIZE 83
+vector<long long> fibonacci(vector<long long>& arr)
+{
+	arr[0] = 1;
+	arr[1] = 1;
+	for (int i = 2; i < MAXSIZE; ++i)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+	return arr;
+}
+//求某一段的和
+long long Sum(vector<long long>& arr, int from, int to)
+{
+	return arr[to + 1] - arr[from];
+}
+int main()
+{
+	int from, to;
+	vector<long long> arr(MAXSIZE);
+	vector<long long> ret= fibonacci(arr);
+	while (cin >> from >> to)
+	{
+		long long result = Sum(ret, from, to);
+		cout << result << endl;
 	}
 	return 0;
 }
